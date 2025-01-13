7c5d09f3fdb737afd04185bd27fecdc399ce27f0
Sameer Kumar <103487647+sameerkumar10@users.noreply.github.com>
2025-01-13 18:29:58 +0530

    Changes for error in routing on vercel 3

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Root of your app
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensures the output directory matches your folder structure
-    emptyOutDir: true, // Clears the output directory before building
-  },
-  server: {
-    port: 3000, // Optional: set the local development server port
-  },
 })
